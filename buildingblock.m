818cbff4c1d49066813488167a6b7c37dec3c5cd
Wout-S <35503601+Wout-S@users.noreply.github.com>
2019-10-30 17:00:16 +0100

    Update buildingblock.m
    rotational slaving mechanism building block added

--- buildingblock.m
+++ buildingblock.m
@@ -26,12 +26,12 @@ classdef buildingblock < handle
                     lambda=bprops.dim(2); % cross point
                     alpha=bprops.dim(1);    % half cross angle
                     p=[kp(1).p; kp(2).p];
-                    h=norm((p(1,:)-p(2,:)));
-                    hdir=(p(1,:)-p(2,:))/h;
-                    L=h/cos(alpha);
-                    wdir=cross(epf.orien,hdir);
-                    w1=lambda*L*sin(alpha);
-                    w2=(1-lambda)*L*sin(alpha);
+                    L=norm((p(1,:)-p(2,:)));
+                    Ldir=(p(1,:)-p(2,:))/L;
+                    L_ls=L/cos(alpha);
+                    wdir=cross(epf.orien,Ldir);
+                    w1=lambda*L_ls*sin(alpha);
+                    w2=(1-lambda)*L_ls*sin(alpha);
                     np=[p(1,:)-w1*wdir;
                         p(1,:)+w1*wdir;
                         p(2,:)+w2*wdir;
@@ -39,12 +39,32 @@ classdef buildingblock < handle
                     kps=[kp data.addkp(np)];
                     rigid=[3 1; 1 4; 5 2; 2 6];
                     flex=[3 5
-                          4 6];
+                        4 6];
                     eflex=data.addelem(kps(flex),epf,bprops.fsect,bprops.fmat);
                     erigid=data.addelem(kps(rigid),epr,bprops.rsect,bprops.rmat);
-                    obj.kp=kps;
-                    obj.elements=[eflex,erigid];
+                case 'rotslav'
+                    dim=bprops.dim;
+                    p=[kp(1).p; kp(2).p; kp(3).p];
+                    L=norm((p(1,:)-p(3,:)));
+                    Ldir=(p(3,:)-p(1,:))/L;
+                    tdir=cross(Ldir,epf.orien);
+                    np=[dim(1)*tdir;
+                        dim(1)*tdir+dim(2)*Ldir;
+                        dim(4)*tdir+dim(2)*Ldir;
+                        dim(3)*tdir+dim(2)*Ldir;
+                        dim(3)*tdir+(L/2)*Ldir;
+                        dim(4)*tdir+(L/2)*Ldir;
+                        dim(1)*tdir+L*Ldir;]+p(1,:);
+                    kps=[kp data.addkp(np)];
+                    rconn=[4 5; 5 6; 6 7; 8 9; 8 2; 5 10];
+                    fconn=[1 4; 6 9; 7 8; 10 3];
+                    eflex=data.addelem(kps(fconn),epf,bprops.fsect,bprops.fmat);
+                    erigid=data.addelem(kps(rconn),epr,bprops.rsect,bprops.rmat);
+                case 'custom'
+                    % custom building block based on a number of matrices
             end
+            obj.kp=kps;
+            obj.elements=[eflex,erigid];
         end
         
     end
